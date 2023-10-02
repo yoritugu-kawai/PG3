ec0a111c625540f5f891f122cbf3f0a6aafef2c7
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-10-02 11:18:52 +0900

    関数テンプレート

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
-#include<stdio.h>
+﻿#include<stdio.h>
+template<typename T>
+
+T Min(T a, T b) {
+	if (a < b) {
+		return a;
+	}
+	if (a > b) {
+		return b;
+	}
+}
+template<>
+char Min<char>(char a, char b) {
+	return printf("数字以外は代入出来ません");
+
+}
+
 
 int main() {
-
-
+	//int型
+	printf("%d or%d = %d\n", 4,5,Min<int>(4, 5));
+	//float型
+	printf("%.1f or %.1f = %.1f\n",4.2f,4.3f, Min<float>(4.2f, 4.3f));
+	//double型
+	printf("%.1lf or%.1lf = %.1lf\n",4.3f,4.1f, Min<double>(4.3f, 4.1f));
+	//char型
+	printf("%c\n", Min<char>(1, 2));
+	return 0;
 }
