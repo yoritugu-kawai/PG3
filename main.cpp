d4a6d1107263d71c95f402fc49aec180aa6ea158
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-10-16 11:35:31 +0900

    k

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 ﻿#include<stdio.h>
 
+int Recursive(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	return(n * Recursive(n-1));
+}
+
+
 int main() {
-	
+	int n = 8;
+	int result;
+	result = Recursive(n);
+	printf("%d=%d\n", n, result);
 	return 0;
 }
