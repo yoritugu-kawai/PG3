7cdc6f7e13bc06ec7da8b290a4bd6ed133e1337d
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-11-06 14:54:03 +0900

    ッ済み

--- main.cpp
+++ main.cpp
@@ -1,7 +1,61 @@
-
 #include<stdio.h>
+#include<random>
+#include<Windows.h>
+#include<functional>
+typedef void (*Func)(int*);
+int GetDiceNumber() {
+	std::random_device random;
+	return random() % 6 + 1;
+}
+
+void Dinghan(int* answer) {
+	
+
+	int rndNum = GetDiceNumber();
+
+
+	printf("‚³‚¢‚±‚ë‚Ì’l‚Í%d‚¾‚æ\n", rndNum);
+
+	if (rndNum % 2 == 0 && *answer % 2 == 0) {
+		printf("³‰ğ!\n");
+	}
+	else if (rndNum % 2 == 1 && *answer % 2 == 1) {
+		printf("³‰ğ!!\n");
+	}
+	else {
+		printf("‚¢‚½‚¾‚«‚â‚·EE\n");
+	}
+}
+
+void Taut(Func p, int answer) {
+	printf("‚³‚ ‚Í‚Á‚½‚Í‚Á‚½\n\n");
+	
+	Sleep(3 * 1000);
+
+
+
+	p(&answer);
+}
 
 int main() {
+
+
+	
+	printf("’š‚È‚ç2‚ğ\n");
+	printf("”¼‚È‚ç1‚ğ\n");
+
+
+	int answer = 0;
+	scanf_s("%d", &answer);
+
+
+	Func wait;
+	wait = Dinghan;
+	//“š‚¦‚ªGetDiceNumber
+	Taut(wait, answer);
+
+
+
 
 	return 0;
 }
