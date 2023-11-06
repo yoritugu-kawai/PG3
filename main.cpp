b5f4bbda34ee99e8f4de83fa4c71e2780bc54810
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-11-06 11:19:14 +0900

    hinmn

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<Windows.h>
 int main() {
-	SetConsoleOutputCP(65001);
-	char str[] = "ソ";
-	printf("%s", str);
+	//SetConsoleOutputCP(65001);
+	
+	printf("ひん\n");
 	return 0;
 }
