2e38e767deb11f6d52c83a5065a26997a07d20fd
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-11-06 15:06:39 +0900

    済み

--- main.cpp
+++ main.cpp
@@ -13,25 +13,22 @@ int GetDiceNumber() {
 int main() {
 
 
-	printf("さいころを振って奇数か偶数を当てるゲーム\n\n");
-	printf("奇数だと思うなら1\n");
-	printf("偶数だと思うなら2を押してね\n");
-
+	printf("丁なら2を\n");
+	printf("半なら1を\n");
 
 	int answer = 0;
 	scanf_s("%d", &answer);
 
-	std::function<void()> SetTimeout = []() {
-		printf("さて答えはなんだろな\n\n");
-		
+	std::function<void()> Taut = []() {
+		printf("さあはったはった\n\n");
 		Sleep(3 * 1000);
 	};
 
 
-	SetTimeout();
+	Taut();
 
 
-	std::function<void(int)> DisplayResult = [](int answer) {
+	std::function<void(int)> Dinghan = [](int answer) {
 	
 
 		int rndNumber = GetDiceNumber();
@@ -40,17 +37,17 @@ int main() {
 		printf("さいころの値は%dだよ\n", rndNumber);
 
 		if (rndNumber % 2 == 0 && answer % 2 == 0) {
-			printf("正解!!\n");
+			printf("丁!!\n");
 		}
 		else if (rndNumber % 2 == 1 && answer % 2 == 1) {
-			printf("正解!!\n");
+			printf("半!!\n");
 		}
 		else {
-			printf("不正解・・\n");
+			printf("終わりだな・・\n");
 		}
 	};
 
-	DisplayResult(answer);
+	Dinghan(answer);
 
 	return 0;
 }
