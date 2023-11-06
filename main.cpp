c51e39745b935a3514401e9064a1d19dcf8f8085
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-11-06 11:01:38 +0900

    済

--- main.cpp
+++ main.cpp
@@ -1,49 +1,8 @@
-﻿#include<stdio.h>
-
-int Recursive(int money ,int time ) {
-	if (time == 1) {
-		return money;
-	}
-	if (time > 1) {
-		return money+ Recursive(money * 2 - 50,time-1);
-	}
-
-}
-
-
+#include<iostream>
+#include<Windows.h>
 int main() {
-	int hoursWorked = 1;
-	int salary = 1072;
-	int normalSalary = 0;
-	int recursionSalary = 0;
-
-	
-	for (int i = 1; i < 10; i++) {
-		//通常
-		normalSalary = salary * hoursWorked;
-
-		//再帰
-		recursionSalary = Recursive(100, hoursWorked);
-
-		printf("%d時間", i);
-		printf("通常だと%d円\n", normalSalary);
-		printf("再帰だと%d円\n\n", recursionSalary);
-
-		if (recursionSalary < normalSalary) {
-
-			hoursWorked++;
-		}
-		if (recursionSalary > normalSalary) {
-			printf("再帰的な賃金が超えました\n");
-			break;
-		}
-	}
-
-
-	printf("よって8時間である\n");
-
-
-
-
+	SetConsoleOutputCP(65001);
+	char str[] = "ソ";
+	printf("%s", str);
 	return 0;
 }
