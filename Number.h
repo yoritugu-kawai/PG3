e3f35e431c20cf4b669ffb1db4d06bc980e4b869
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-11-12 23:04:21 +0900

    1

---
+++ Number.h
@@ -0,0 +1,29 @@
+#pragma once
+
+template<typename T1,typename T2>
+class NumberCompare
+{
+public:
+	NumberCompare() {};
+	~NumberCompare() {};
+
+	void SetNumber(T1 n1,T2 n2)
+	{
+		n1_ = n1;
+		n2_ = n2;
+	}
+
+	T1 Min() 
+	{
+		if (n1_ < n2_)
+		{
+			return n1_;
+		}
+		return static_cast<T1>(n2_);
+	}
+
+private:
+
+	T1 n1_{};
+	T2 n2_{};
+};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include"NumberCompare.h"
+﻿#include"Number.h"
 #include<stdio.h>
 #include<iostream>
 
@@ -6,35 +6,34 @@ using namespace std;
 
 int main()
 {
-	//int,int
-	NumberCompare<int, int>Int_IntCompare;
-	Int_IntCompare.SetNumber(2, 4);
-	cout << Int_IntCompare.Min() << endl;
+	
+	Number<int, int>int_Int;
+	int_Int.SetNumber(2, 4);
+	cout << int_Int.Min() << endl;
 
-	//int,float
-	NumberCompare<int, float>Int_FloatCompare;
-	Int_FloatCompare.SetNumber(6, 4.0f);
-	cout << Int_FloatCompare.Min() << endl;
+	
+	Number<int, float>int_Float;
+	int_Float.SetNumber(6, 4.0f);
+	cout << int_Float.Min() << endl;
 
-	//int,double
-	NumberCompare<int, double>Int_DoubleCompare;
-	Int_DoubleCompare.SetNumber(7, 6.0);
-	cout << Int_DoubleCompare.Min() << endl;
+	
+	Number<int, double>int_Double;
+	int_Double.SetNumber(7, 6.0);
+	cout << int_Double.Min() << endl;
 
-	//float float
-	NumberCompare<float, float>Float_FloatCompare;
-	Float_FloatCompare.SetNumber(2.0f, 4.0f);
-	cout << Float_FloatCompare.Min() << endl;
+	
+	Number<float, float>float_Float;
+	float_Float.SetNumber(2.0f, 4.0f);
+	cout << float_Float.Min() << endl;
 
-	//float,double
-	NumberCompare<float, double>Float_DoubleCompare;
-	Float_DoubleCompare.SetNumber(2.0f, 4.0);
-	cout << Float_DoubleCompare.Min() << endl;
+	Number<float, double>float_Double;
+	float_Double.SetNumber(2.0f, 4.0);
+	cout << float_Double.Min() << endl;
 
-	//double,double
-	NumberCompare<double, double>Double_DoubleCompare;
-	Double_DoubleCompare.SetNumber(7.0, 3.0);
-	cout << Double_DoubleCompare.Min() << endl;
+	
+	Number<double, double>double_Double;
+	double_Double.SetNumber(7.0, 3.0);
+	cout << double_Double.Min() << endl;
 
 	return 0;
 }
