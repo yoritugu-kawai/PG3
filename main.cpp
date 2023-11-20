ec38bf50a2765d9beaca52a93c2d62ef85cd634d
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-11-20 10:58:03 +0900

    1

--- main.cpp
+++ main.cpp
@@ -11,43 +11,37 @@ int GetDiceNumber() {
 	std::random_device rnd;
 	return rnd() % 6 + 1;
 }
-std::function<void()> SetTimeout = []() {
+void SetTimeout () {
 	printf("さあはったはった\n\n");
 	Sleep(3 * 1000);
 };
 
+std::function<void(int)> Dinghan = [=](int answer) {
+
+
+	int rndNumber = GetDiceNumber();
+
+
+	printf("さいころの目は%dだ\n", rndNumber);
+
+	if (rndNumber % 2 == 0 && answer % 2 == 0) {
+		printf("丁!!\n");
+	}
+	else if (rndNumber % 2 == 1 && answer % 2 == 1) {
+		printf("半!!\n");
+	}
+	else {
+		printf("終わりだな・・\n");
+	}
+};
 int main() {
-	SetConsoleOutputCP(65001);
+	//SetConsoleOutputCP(65001);
 
 	printf("丁なら2を\n");
 	printf("半なら1を\n");
-
 	int answer = 0;
 	scanf_s("%d", &answer);
-
-
-
 	SetTimeout();
-
-
-	std::function<void(int)> Dinghan = [](int answer) {
-	
-
-		int rndNumber = GetDiceNumber();
-
-
-		printf("さいころの値は%dだよ\n", rndNumber);
-
-		if (rndNumber % 2 == 0 && answer % 2 == 0) {
-			printf("丁!!\n");
-		}
-		else if (rndNumber % 2 == 1 && answer % 2 == 1) {
-			printf("半!!\n");
-		}
-		else {
-			printf("終わりだな・・\n");
-		}
-	};
 
 	Dinghan(answer);
 
