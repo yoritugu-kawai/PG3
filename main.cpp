d39fc7be3bf439ab398a17a307d108b1684ce20d
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-02-11 01:38:46 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -2,60 +2,54 @@
 #include<random>
 #include<Windows.h>
 #include<functional>
-typedef void (*Func)(int*);
-int GetDiceNumber() {
-	std::random_device random;
-	return random() % 6 + 1;
+#include <iostream>
+
+using namespace std;
+
+std::random_device rd;
+std::mt19937 mt(rd());
+
+int Dice() {
+	return mt() % 6 + 1;
 }
 
-void Dinghan(int* answer) {
-	
+typedef void (*pFunction)(int a, int b);
 
-	int rndNum = GetDiceNumber();
+void SetTimeOut(pFunction function, int second, int diceRolling, int player) {
+	Sleep(second * 1000);
+	function(diceRolling, player);
+}
 
+void Result(int diceRolling, int player) {
+	cout << "サイコロの出目は" << diceRolling << "で";
+	cout << (diceRolling % 2 == 0 ? "偶数" : "奇数") << "だ" << endl;
 
-	printf("さいころの値は%dだよ\n", rndNum);
-
-	if (rndNum % 2 == 0 && *answer % 2 == 0) {
-		printf("丁!\n");
-	}
-	else if (rndNum % 2 == 1 && *answer % 2 == 1) {
-		printf("半!!\n");
+	if ((diceRolling % 2 == 0 && player == 2) || (diceRolling % 2 == 1 && player == 1)) {
+		cout << "当たり" << endl;
 	}
 	else {
-		printf("いただきやす・・\n");
+		cout << "外れ" << endl;
 	}
 }
 
-void Taut(Func p, int answer) {
-	printf("さあはったはった\n\n");
-	
-	Sleep(3 * 1000);
+int PlayerChallenge() {
+	int player = 0;
 
+	while (player != 1 && player != 2) {
+		cout << "奇数か偶数を選択してEnterを押せ!!" << std::endl;
+		cout << "1 : 奇数, 2 : 偶数" << endl;
+		cin >> player;
+	}
 
-
-	p(&answer);
+	return player;
 }
 
-int main() {
+int main(void) {
+	int player = PlayerChallenge();
+	int diceRolling = Dice();
 
-
-	
-	printf("丁なら2を\n");
-	printf("半なら1を\n");
-
-
-	int answer = 0;
-	scanf_s("%d", &answer);
-
-
-	Func Okay;
-	Okay = Dinghan;
-	//答えがGetDiceNumber
-	Taut(Okay, answer);
-
-
-
+	pFunction result = Result;
+	SetTimeOut(result, 3, diceRolling, player);
 
 	return 0;
 }
