47befa75b1a30f1177ba012f589b48e20ba1c7d7
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2024-02-11 02:09:07 +0900

    完

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 
 
 int main(void) {
-	const size_t kShape = 2;
-	IShape* shape[kShape];
+	const size_t count = 2;
+	IShape* shape[count];
 
 	shape[0] = new Circle(8.0f);
 	shape[1] = new Rectangle(4.0f, 11.0f);
 
-	for (size_t i = 0; i < kShape; i++) {
+	for (size_t i = 0; i < count; i++) {
 		shape[i]->Size();
 		shape[i]->Draw();
 	}
 
-	for (size_t i = 0; i < kShape; i++) {
+	for (size_t i = 0; i < count; i++) {
 		delete shape[i];
 	}
 
